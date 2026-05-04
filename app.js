1deb33be422a77ec1664077f40454c0760046501
harsha <hc20040621@gmail.com>
2026-05-04 16:07:26 +0530

    Implement server-side proxy to fix mixed content

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
+const axios = require('axios'); // We need axios here
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 8080;
 
-// This is the IP of your GKE Backend
 const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:5000';
 
+// This serves your HTML
 app.get('/', (req, res) => {
-  // Read the static HTML file
-  let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-  
-  // Inject the real Backend IP into the Javascript in the HTML
-  html = html.replace("BACKEND_URL_STR", BACKEND_URL);
-  
-  res.send(html);
+  res.sendFile(__dirname + '/index.html');
+});
+
+// NEW: This acts as a proxy bridge
+app.get('/api/products', async (req, res) => {
+  const response = await axios.get(`${BACKEND_URL}/api/products`);
+  res.json(response.data);
+});
+
+app.get('/api/cart', async (req, res) => {
+  const response = await axios.get(`${BACKEND_URL}/api/cart`);
+  res.json(response.data);
+});
+
+app.post('/api/cart', async (req, res) => {
+  const response = await axios.post(`${BACKEND_URL}/api/cart`, req.body);
+  res.json(response.data);
+});
+
+app.delete('/api/cart', async (req, res) => {
+  const response = await axios.delete(`${BACKEND_URL}/api/cart`);
+  res.json(response.data);
 });
 
 app.listen(port);
